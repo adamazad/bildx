d44eac59bc0929dfbc014ece2721a98a272bfba5
Adam Azad <adam@adamazad.com>
2020-09-18 17:19:08 +0300

    ✅ Use temp cache directory

--- tests/bild.ts
+++ tests/bild.ts
@@ -1,9 +1,13 @@
+import Fs from 'fs';
 import Path from 'path';
 import Sharp from 'sharp';
 import request from 'supertest';
 import { BildX, BildXStorage } from '../src';
 
 describe('BildX', () => {
+  // Create empty cache dir
+  const CACHE_TEMP_DIR = Path.resolve(__dirname, Fs.mkdtempSync('./tests/cache-'));
+
   const BILDX_PORT = 5000;
   const testImageName = 'joshua-rondeau-ecScGr6Oicw-unsplash.jpg';
   const testImagePath = Path.resolve(__dirname, './storage/', testImageName);
@@ -11,7 +15,7 @@ describe('BildX', () => {
   // Create an instance
   const bildX = new BildX({
     storage: new BildXStorage(Path.resolve(__dirname, './storage')),
-    cache: new BildXStorage(Path.resolve(__dirname, './cache')),
+    cache: new BildXStorage(CACHE_TEMP_DIR),
   });
 
   test('BildX.storage and BildX.cache should be of type BildXStorage', () => {
@@ -26,7 +30,7 @@ describe('BildX', () => {
   describe('Original image request', () => {
     test('Should be of type image/jpeg', async () => {
       const res = await request(bildX.http).get(`/${testImageName}`);
-      expect(res.type).toMatch(/image\/jpeg/);
+      expect(res.type).toBe('image/jpeg');
     });
 
     test('Dimensions should match the original', async () => {
