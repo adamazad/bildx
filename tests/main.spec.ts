7a1e4675c1caa9df167d3d1bbc61c206400c10cc
Adam Azad <adam@adamazad.com>
2020-09-18 19:18:22 +0300

    ✅ Rename

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,12 +1,9 @@
 import Path from 'path';
-import Sharp from 'sharp';
-import request from 'supertest';
 import { BildX, BildXStorage } from '../src';
 
 describe('BildX', () => {
   const BILDX_PORT = 5000;
-  const testImageName = 'joshua-rondeau-ecScGr6Oicw-unsplash.jpg';
-  const testImagePath = Path.resolve(__dirname, './storage/', testImageName);
+  // const testImagePath = Path.resolve(__dirname, './storage/', testImageName);
   let bildX: BildX;
 
   beforeAll(() => {
@@ -28,18 +25,5 @@ describe('BildX', () => {
 
   test(`BildX.start should start listening on ${BILDX_PORT}`, () => {
     expect(bildX.start(BILDX_PORT)).resolves.toBeCalled();
-  });
-
-  describe('Original image request', () => {
-    test('Dimensions should match the original', async () => {
-      const res = await request(bildX.http).get(`/${testImageName}`);
-      // Original file
-      const testImageMetadata = await Sharp(testImagePath).metadata();
-      // Response
-      const { height, width } = await Sharp(res.body).metadata();
-      // Match the dimensions
-      expect(width).toEqual(testImageMetadata.width);
-      expect(height).toEqual(testImageMetadata.height);
-    });
   });
 });
