114c85e7679cc444e1d6dc736e395bdcf282df79
Adam Azad <adam@adamazad.com>
2020-09-18 17:50:55 +0300

    ✅ Use ./tests/cache directory

--- tests/bild.ts
+++ tests/bild.ts
@@ -1,21 +1,24 @@
-import Fs from 'fs';
 import Path from 'path';
 import Sharp from 'sharp';
 import request from 'supertest';
 import { BildX, BildXStorage } from '../src';
 
 describe('BildX', () => {
-  // Create empty cache dir
-  const CACHE_TEMP_DIR = Path.resolve(__dirname, Fs.mkdtempSync('./tests/cache-'));
-
   const BILDX_PORT = 5000;
   const testImageName = 'joshua-rondeau-ecScGr6Oicw-unsplash.jpg';
   const testImagePath = Path.resolve(__dirname, './storage/', testImageName);
+  let bildX: BildX;
 
-  // Create an instance
-  const bildX = new BildX({
-    storage: new BildXStorage(Path.resolve(__dirname, './storage')),
-    cache: new BildXStorage(CACHE_TEMP_DIR),
+  beforeAll(() => {
+    // Create an instance
+    bildX = new BildX({
+      storage: new BildXStorage(Path.resolve(__dirname, './storage')),
+      cache: new BildXStorage(Path.resolve(__dirname, './cache')),
+    });
+  });
+
+  afterAll(async () => {
+    await bildX.stop();
   });
 
   test('BildX.storage and BildX.cache should be of type BildXStorage', () => {
@@ -28,11 +31,6 @@ describe('BildX', () => {
   });
 
   describe('Original image request', () => {
-    test('Should be of type image/jpeg', async () => {
-      const res = await request(bildX.http).get(`/${testImageName}`);
-      expect(res.type).toBe('image/jpeg');
-    });
-
     test('Dimensions should match the original', async () => {
       const res = await request(bildX.http).get(`/${testImageName}`);
       // Original file
